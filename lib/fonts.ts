0a4a9a08bec2922300c887ee31a8de53c8454581
clinton <clintonphillips464@gmail.com>
2024-12-05 15:33:39 +0100

    remove unused cinzel import

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,3 +1,3 @@
 
-import {   Cinzel_Decorative, Cinzel } from "next/font/google";
+import {   Cinzel_Decorative } from "next/font/google";
 export const cinzel = Cinzel_Decorative({ subsets: ["latin"], weight: [ "400" , "700",  "900"] });
